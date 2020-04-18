a318daa5bb69ee3cd51bf00ca9dadb7d797ad35f
Kartik Ayyer <kartik.ayyer@desy.de>
2020-04-18 06:41:24 +0200

    plot_utils: Renamed plot_diag to plot_partials and allowing non-diagonal components

--- plot_utils.py
+++ plot_utils.py
@@ -3,9 +3,13 @@ import pylab as P
 from scipy import ndimage
 from skopt import plots
 
-def plot_diag(res, d1=True, d2=True, ylim=None):
-    num_vecs = int((np.sqrt(8*len(res.x)+1) - 1)/2.)
-    diags = [i*(i+3)//2 for i in range(num_vecs)]
+def plot_partials(res, diag=True, d1=True, d2=True, ylim=None):
+    if diag:
+        num_vecs = int((np.sqrt(8*len(res.x)+1) - 1)/2.)
+        diags = [i*(i+3)//2 for i in range(num_vecs)]
+    else:
+        num_vecs = len(res.x)
+        diags = np.arange(len(res.x))
     if d1:
         for k in range(len(diags)):
             P.subplot(num_vecs, num_vecs, k*num_vecs + k + 1)
@@ -25,12 +29,13 @@ def plot_diag(res, d1=True, d2=True, ylim=None):
             for j in range(i):
                 P.subplot(num_vecs, num_vecs, i*num_vecs + j + 1)
                 P.cla()
-                P.contourf(*plots.partial_dependence(res.space, res.models[-1], diags[i], diags[j], n_points=10), cmap='gist_earth_r', levels=levels)
+                P.contourf(*plots.partial_dependence(res.space, res.models[-1], diags[i], diags[j], n_points=10, n_samples=100), cmap='gist_earth_r', levels=levels)
                 P.text(0.5, 0.9, '(%d, %d)'%(diags[j], diags[i]), horizontalalignment='center', transform=P.gca().transAxes, c='r')
                 P.axis('off')
                 print((diags[i],diags[j]))
+    P.subplots_adjust(left=0.01, right=0.91, bottom=0.01, top=0.91)
 
-def plot_conv(res)
+def plot_conv(res):
     plots.plot_convergence(res)
     P.plot(np.arange(len(res.func_vals))+1, ndimage.gaussian_filter1d(res.func_vals, 4), c='C1')
     P.plot(np.arange(len(res.func_vals))+1, res.func_vals, ls='None', marker='+', c='C0')
