a30d251b32c39d78a7fbf3557cde52284cd0d0a9
Parichita Mazumder <parichita.mazumder@mpsd.mpg.de>
2020-05-08 17:42:51 +0200

    CC v/s q: Calculate Imc and CC with Itarget & plot CC v/s q.

--- plot_CC_q.py
+++ plot_CC_q.py
@@ -6,16 +6,19 @@ import skopt
 from skopt import load
 import matplotlib.pylab as P
 import h5py
+import os
+import os.path as op
 
 def main():
     import argparse
     parser = argparse.ArgumentParser(description=' 1. Imc calculator with BGO optimized s vectors; 2. Estimate anisotropic CC between Itarget and Imc; 3. Plotting CC v/s q')
     parser.add_argument('-c','--config_file', help ='Config file')
     parser.add_argument('-mc', '--run_mc', help = 'Calculate Imc with BGO optimized  s vectors. Default = False', action ='store_true')
-    parser.add_argument('-o', '--save',  help = 'Saving Imc and CC')
-    parser.add_argument('Imc', help='Path to Imc file already exist')
+    parser.add_argument('Imc', help='Path to Imc file already exist.')
+    parser.add_argument('-cc', '--calc_cc', help ='calculate CC between Itarget and Imc. Default = False', action = 'store_true')
+    parser.add_argument('CC_q', help ='Path to CC/q dat file.')
+
     parser.add_argument('-i', '--input_file/input_files', help = 'Files for compare CC v/s q plot from different Imc dataset and for recalculation of CC') 
-    parser.add_argument('-l', '--label', help='define label for plot. Default = out_fname.')
     
     parser.add_argument('-d', '--device', help = 'GPU device ID(if applicable). Default:0', type = int, default=0)
     
@@ -29,7 +32,8 @@ def main():
     pcd = opt.pcd
     num_vecs = opt.num_vecs
     #get S vecs(cov_weights) for run_mc() from BGO optimization
-    res = skopt.load(opt.output_fname)
+    output_fname = opt.output_fname
+    res = skopt.load(output_fname)
     s = np.array(res.x)
 
     # get only the diagonals of cov_weights
@@ -44,6 +48,10 @@ def main():
     if args.run_mc: 
         Imc = opt.get_mc_intens(s)
         #Imc = opt.get_mc_intens(sdiag)
+        
+        with h5py.File(op.splitext(output_fname)[0]+'_diffcalc.h5', 'w') as f:    
+             f['diff_intens'] = Imc
+
     else:
         #get Imc from file using -i option
         with h5py.File(args.Imc, 'r') as f:
@@ -53,50 +61,48 @@ def main():
     Itarget = opt.Itarget
     
     ###CC between Itarget and Imc
+    if args.calc_cc:
+        Vol1 = cp.array(Itarget)
+        Vol2 = cp.array(Imc)
 
-    Vol1 = cp.array(Itarget)
-    Vol2 = cp.array(Imc)
+        assert Vol1.shape == Vol2.shape
+        size = Vol1.shape[-1]
+        bin_size = 1
+        rad, rbin = calc_cc.calc_rad(size, bin_size)
+        num_bins = int(rbin.max() +1)
 
-    assert Vol1.shape == Vol2.shape
-    size = Vol1.shape[-1]
-    bin_size = 1
-    rad, rbin = calc_cc.calc_rad(size, bin_size)
-    num_bins = int(rbin.max() +1)
+        calc_cc.subtract_radavg(Vol1, rbin, num_bins)
+        calc_cc.subtract_radavg(Vol2, rbin, num_bins) 
 
-    calc_cc.subtract_radavg(Vol1, rbin, num_bins)
-    calc_cc.subtract_radavg(Vol2, rbin, num_bins)
-
-    cc = calc_cc.calc_cc(Vol1, Vol2, rbin, num_bins)
+        cc = calc_cc.calc_cc(Vol1, Vol2, rbin, num_bins)
+        cc = cc.get()    
+        # cal q
+        res_edge = 1.35
+        cen = size //2
+        q = np.arange(num_bins) * bin_size / cen / res_edge
         
-    # cal q
-    res_edge = 1.35
-    cen = size //2
-    q = np.arange(num_bins) * bin_size / cen / res_edge
+        calc_cc.save_to_file(op.splitext(output_fname)[0] +'_diffcal_CC.dat', cc, q)
+    else:
+        Corr = np.loadtxt(args.CC_q, skiprows =1)
+        q = Corr[:, 1]
+        cc = Corr[:, 2]
+        out_fname  = args.CC_q
     
-    #Save Imc, CC 
-    with h5py.File(opt.output_fname+'_diffcalc_cc.h5', 'w') as f:    #need to edit  file name
-         f['diff_intens'] = Imc
-         f['cc'] = cc.get()
-         f['q'] = q
-
-
-
     ##Plotting CC v/s q
     ax = P.axes(xlim=(0, 1.4), ylim=(0, 1.0))
 
-    P.plot(q, cc, 'r-' )
+    P.plot(q, cc, 'r-')
     P.xlabel('q [1/$\AA$]')
     P.ylabel ('CC')
-    P.title('CC v/s q')
+   # P.title(str(op.splitext(output_fname)[0][24: ]))
     #P.axvline(q[10], ls='--', c = 'k')
     #P.axvline(q[100], ls='--', c = 'k')
     #P.axvline(q[200], ls ='--', c = 'k')
 
     P.grid()
-    P.legend()
+    #P.legend()
     P.show()
-    #P.savefig('../CypA/Analysis/output_fname.png')
-
+#    P.savefig('../CypA/Analysis/' + op.splitext(output_fname)[0][24: ] + '.png')
 
 
 if __name__== '__main__':
