aa6991b59414e2743e1174c0285b11efa1cf973a
Parichita Mazumder <parichita.mazumder@mpsd.mpg.de>
2020-05-12 10:30:31 +0200

    CC v/s q:Added options for CC calculation and plotting of single dataset.

--- plot_CC_q.py
+++ plot_CC_q.py
@@ -12,13 +12,11 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description=' 1. Imc calculator with BGO optimized s vectors; 2. Estimate anisotropic CC between Itarget and Imc; 3. Plotting CC v/s q')
-    parser.add_argument('-c','--config_file', help ='Config file')
+    parser.add_argument('config_file', help ='Path to config file')
     parser.add_argument('-mc', '--run_mc', help = 'Calculate Imc with BGO optimized  s vectors. Default = False', action ='store_true')
-    parser.add_argument('-Imc', '--Imc', help='Path to Imc file already exist. Default = False', action = 'store_true')
-    parser.add_argument('-CC_q', '--CC_q', help ='Path to CC/radius/q file.')
-
-   # parser.add_argument('-i', '--input_file/input_files', help = 'Files for compare CC v/s q plot from different Imc dataset and for recalculation of CC') 
-    
+    parser.add_argument('-i', '--diff_intens', help='Path to Imc diff_intens files.')
+    parser.add_argument('-c', '--corr_q', help ='Path to cc/radius/q file.')
+    parser.add_argument('-o', '--out_fname', help ='Path to output files.')
     parser.add_argument('-d', '--device', help = 'GPU device ID(if applicable). Default:0', type = int, default=0)
     
     args = parser.parse_args()
@@ -30,7 +28,7 @@ def main():
         
     pcd = opt.pcd
     num_vecs = opt.num_vecs
-    #get S vecs(cov_weights) for run_mc() from BGO optimization
+    #get s vecs(cov_weights) for run_mc() from BGO optimization
     output_fname = opt.output_fname
     res = skopt.load(output_fname)
     s = np.array(res.x)
@@ -43,26 +41,16 @@ def main():
         sdiag.append(d)
     sdiag = np.array(sdiag)
     #print(sdiag)
-    ''' 
-    if args.run_mc: 
-        Imc = opt.get_mc_intens(s)
-        #Imc = opt.get_mc_intens(sdiag)
-        
-        with h5py.File(op.splitext(output_fname)[0]+'_diffcalc.h5', 'w') as f:    
-             f['diff_intens'] = Imc
+    '''
+    #get Itarget
+    Itarget = opt.Itarget
 
-    elif args.Imc:
-        #get Imc from file using -i option
-        with h5py.File(args.Imc, 'r') as f:
-            Imc = f['diff_intens'][:]
-
-        #get Itarget
-        Itarget = opt.Itarget
-    
+    def get_cc_q(Imc, Itarget):
         ###CC between Itarget and Imc
         Vol1 = cp.array(Itarget)
         Vol2 = cp.array(Imc)
-
+        
+        #cal cc
         assert Vol1.shape == Vol2.shape
         size = Vol1.shape[-1]
         bin_size = 1
@@ -74,34 +62,62 @@ def main():
 
         cc = calc_cc.calc_cc(Vol1, Vol2, rbin, num_bins)
         cc = cc.get()    
+        
         # cal q
         res_edge = 1.35
         cen = size //2
         q = np.arange(num_bins) * bin_size / cen / res_edge
-        
-        calc_cc.save_to_file(op.splitext(output_fname)[0] +'_diffcal_CC.dat', cc, q)
+        return(cc, q)
 
-    else:
-        Corr = np.loadtxt(args.CC_q, skiprows =1)
-        q = Corr[:, 1]
-        cc = Corr[:, 2]
+
+
+    if args.run_mc: 
+        #calculate Imc and CC between Itarget
+        Imc = opt.get_mc_intens(s)
+        #Imc = opt.get_mc_intens(sdiag)
+
+        print('Writing Imc to', op.splitext(output_fname)[0]+'_diffcalc.h5')
+        with h5py.File(op.splitext(output_fname)[0]+'_diffcalc.h5', 'w') as f:    
+             f['diff_intens'] = Imc
+       
+        cc, q = get_cc_q(Imc, Itarget)
+
+        #saving cc/q
+        if args.out_fname:
+           calc_cc.save_to_file(args.out_fname, cc, q)
+        else:   
+           calc_cc.save_to_file(op.splitext(output_fname)[0] +'_diffcalc_CC.dat', cc, q)
     
+    elif args.diff_intens:
+        #get Imc from saved file usindg -i option, calculate CC between Itarget
+        with h5py.File(args.diff_intens, 'r') as f:
+            Imc = f['diff_intens'][:]
+       
+        cc, q = get_cc_q(Imc, Itarget)  
+        fname = args.diff_intens 
+        
+        #saving cc/q
+        if args.out_fname:
+            calc_cc.save_to_file(args.out_fname, cc, q)
+        else:    
+            calc_cc.save_to_file(op.splitext(fname)[0] +'_CC.dat', cc, q)
+    
+    else:
+        #get cc and q from saved file using -c in command line 
+        corr = np.loadtxt(args.corr_q, skiprows =1)
+        q = corr[:, 1]
+        cc = corr[:, 2]
+        fname = args.corr_q
     
     ##Plotting CC v/s q
     ax = P.axes(xlim=(0, 1.4), ylim=(0, 1.0))
-
     P.plot(q, cc, 'r-')
     P.xlabel('q [1/$\AA$]')
     P.ylabel ('CC')
-   # P.title(str(op.splitext(output_fname)[0][24: ]))
-    #P.axvline(q[10], ls='--', c = 'k')
-    #P.axvline(q[100], ls='--', c = 'k')
-    #P.axvline(q[200], ls ='--', c = 'k')
-
+    P.title('CC v/s q')
     P.grid()
-    #P.legend()
     P.show()
-#    P.savefig('../CypA/Analysis/' + op.splitext(output_fname)[0][24: ] + '.png')
+
 
 
 if __name__== '__main__':
@@ -115,8 +131,3 @@ if __name__== '__main__':
 
 
 
-# save to file
-
-#calc_cc.save_to_file('../CypA/xtc/CC_md295_Imd_Ibgo_5vecs_diag1.dat', cc3, q)
-
-#c10 = np.loadtxt('../CypA/xtc/CC_md295_Imd_Ibgo_10vecs_full_1_wosub.dat', skiprows =1)
