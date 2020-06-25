32582d5f39d5e546198db6c55aa0f14ba24f0f5c
Parichita Mazumder <parichita.mazumder@mpsd.mpg.de>
2020-06-25 15:29:37 +0200

    meanpos: Added command line option

--- meanpos.py
+++ meanpos.py
@@ -2,13 +2,27 @@ import numpy as np
 import cupy as cp
 import MDAnalysis as md
 import h5py
+import argparse
 
-u = md.Universe("../CypA/xtc/ini.tpr", "../CypA/xtc/md295_fit.xtc")
+def main():
+    parser = argparse.ArgumentParser(description = 'Genarating Protein mean position pdb')
+    parser.add_argument('-s', '--tpr', help = 'path to tpr file')
+    parser.add_argument('-f', '--xtc', help = 'path to xtc file')
+    parser.add_argument('-c', '--cov', help = 'path to displacement covariance file')
+    parser.add_argument('-o', '--out_pdb', help = 'path to output pdb file')
 
-p = u.select_atoms("protein")
-f = h5py.File("../CypA/xtc/md295_fit_diffcalc_cov.h5", "r")
-p_avg = f['mean_pos'][:]
+    args = parser.parse_args()
 
-p.positions = p_avg
+    u = md.Universe(args.tpr, args.xtc) #"data/Lysozyme/lyso_crys.tpr", "data/Lysozyme/lyso295_crys800ns_fit.xtc")
 
-p.write("../CypA/xtc/md295_meanpos.pdb")
+    p = u.select_atoms("protein")
+    
+    with h5py.File(args.cov, 'r') as f:
+            p_avg = f['mean_pos'][:]
+
+    p.positions = p_avg
+
+    p.write(args.out_pdb)
+
+if __name__ == '__main__':
+    main()
