61fc2c318a8f2c7b300de4ac3492416da4d16a53
Kartik Ayyer <kartik.ayyer@desy.de>
2020-12-17 00:47:41 +0100

    utils: Removed plotting functions and satisfied linter

--- diffuser/utils.py
+++ diffuser/utils.py
@@ -1,52 +1,26 @@
+import sys
 import numpy as np
-import pylab as P
 import cupy as cp
 
-def plot_cc(cc):
-    P.clf()
-    titles = 'xx yy zz xy yz zx'.split()
-    for i in range(6):
-        s = P.subplot(2,3,i+1)
-        im = P.imshow(cc[i], vmin=-1, vmax=1, cmap='coolwarm')
-        P.title(r'CC$_{%s}$'%titles[i], fontsize=12)
-    P.suptitle('CC matrices', fontsize=14)
-    P.subplots_adjust(right=0.88)
-    cax = P.gcf().add_axes([0.9,0.125,0.02,0.75])
-    P.colorbar(im, cax=cax)
-    
-def plot_ccpos(cc, mean_pos, ind):
-    P.clf()
-    titles = 'xx yy zz xy yz zx'.split()
-    for i in range(6):
-        s = P.subplot(2,3,i+1)
-        im = P.scatter(mean_pos[:,0], mean_pos[:,1], 3, c=cc[i,ind], cmap='coolwarm', vmin=-1, vmax=1)
-        P.plot(mean_pos[ind,0], mean_pos[ind,1], marker='o', markersize=14, color='green', markerfacecolor='none')
-        if i > 2:
-            P.xlabel(r'X ($\AA$)')
-        if i % 3 == 0:
-            P.ylabel(r'Y ($\AA$)')
-        P.title(r'CC_${%s}$'%titles[i])
-    P.subplots_adjust(right=0.88)
-    cax = P.gcf().add_axes([0.9,0.125,0.02,0.75])
-    P.colorbar(im, cax=cax)
-    
 def norm_cov(cov):
-    cc = np.copy(cov)
+    '''Normalize covariance matrix to get CC matrix'''
+    ccmat = np.copy(cov)
     ind = np.arange(cov.shape[-1])
     if len(cov.shape) == 3:
         std = np.sqrt(cov[:3, ind, ind])
-        cc[0] /= np.outer(std[0], std[0])
-        cc[1] /= np.outer(std[1], std[1])
-        cc[2] /= np.outer(std[2], std[2])
-        cc[3] /= np.outer(std[0], std[1])
-        cc[4] /= np.outer(std[1], std[2])
-        cc[5] /= np.outer(std[2], std[0])
+        ccmat[0] /= np.outer(std[0], std[0])
+        ccmat[1] /= np.outer(std[1], std[1])
+        ccmat[2] /= np.outer(std[2], std[2])
+        ccmat[3] /= np.outer(std[0], std[1])
+        ccmat[4] /= np.outer(std[1], std[2])
+        ccmat[5] /= np.outer(std[2], std[0])
     else:
         std = np.sqrt(cov[ind, ind])
-        cc /= np.outer(std, std)
-    return cc
-    
+        ccmat /= np.outer(std, std)
+    return ccmat
+
 def get_allcov(cov):
+    '''Convert (3, N, N) cov matrix to (3N, 3N) cov matrix'''
     allcov = np.zeros((3,cov.shape[1],3,cov.shape[2]))
     allcov[0,:,0] = cov[0]
     allcov[0,:,1] = cov[3].T
@@ -59,14 +33,15 @@ def get_allcov(cov):
     allcov[2,:,2] = cov[2]
     return allcov.reshape(cov.shape[1]*3, cov.shape[2]*3)
 
-def get_densproj(td, mean_pos, fvec, mtype='traj', size=301):
+def get_densproj(tdiff, mean_pos, fvec, mtype='traj', size=301):
+    '''Get Z-projection of distorted molecule's electron density'''
     if mtype not in ['traj', 'linear', 'raw']:
         raise ValueError('mtype must be one of traj, linear or raw')
 
     if mtype == 'linear':
         num_frames = 100
     else:
-        num_frames = len(td.univ.trajectory)
+        num_frames = len(tdiff.univ.trajectory)
 
     densproj = cp.zeros((num_frames, size, size), dtype='f4')
     dens = cp.zeros(3*(size,), dtype='f4')
@@ -74,34 +49,34 @@ def get_densproj(td, mean_pos, fvec, mtype='traj', size=301):
 
     for i in range(num_frames):
         if mtype == 'traj':
-            td.univ.trajectory[i]
-            pos = td.atoms.positions - mean_pos
+            _ = tdiff.univ.trajectory[i]
+            pos = tdiff.atoms.positions - mean_pos
             dpos = np.dot(pos.T.ravel(), fvec) * tfvec
         elif mtype == 'linear':
             dpos = np.linspace(-20, 20, num_frames)[i] * tfvec
         elif mtype == 'raw':
-            td.univ.trajectory[i]
-            dpos = td.atoms.positions - mean_pos
+            _ = tdiff.univ.trajectory[i]
+            dpos = tdiff.atoms.positions - mean_pos
         pos = cp.array(mean_pos + dpos).astype('f4')
         pos /= 0.5 # 0.5 A voxel size
         pos += size//2
         dens[:] = 0
-        td.k_gen_dens((len(mean_pos)//32+1,), (32,),
-                      (pos, td.atom_f0, len(mean_pos), size, dens))
+        tdiff.k_gen_dens((len(mean_pos)//32+1,), (32,),
+                      (pos, tdiff.atom_f0, len(mean_pos), size, dens))
         densproj[i] = dens.sum(2)
     return densproj
-    
-def get_projections(td, mean_pos, vec):
-    num_frames = len(td.univ.trajectory)
+
+def get_projections(tdiff, mean_pos, vec):
+    '''Get projections of trajectory along PC vectors'''
+    num_frames = len(tdiff.univ.trajectory)
     proj = np.zeros((num_frames, vec.shape[1]))
 
     for i in range(num_frames):
-        td.univ.trajectory[i]
-        pos = td.atoms.positions
+        _ = tdiff.univ.trajectory[i]
+        pos = tdiff.atoms.positions
         pos -= pos.mean(0)
         pos -= mean_pos
         proj[i] = np.dot(pos.T.ravel(), vec)
         sys.stderr.write('\r%d/%d'%(i+1, num_frames))
     sys.stderr.write('\n')
     return proj
-
