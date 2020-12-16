c78aa1b1f0489fe87996e09f9d8b197bc75bd512
Kartik Ayyer <kartik.ayyer@desy.de>
2020-12-17 00:35:19 +0100

    setup: Added console_scripts with Phenix-like syntax

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name='diffuser',
-    packages=find_packages(),
+setup(name = 'diffuser',
+    version = '0.1',
+    packages = ['diffuser'],
+    entry_points = {'console_scripts': [
+        'diffuser.bgo_optimize = diffuser.bgo_optimize:main',
+        'diffuser.pcdiff = diffuser.pcdiff:main',
+        'diffuser.traj_diff = diffuser.traj_diff:main',
+        'diffuser.rbdiff = diffuser.rbdiff:main',
+      ],
+    },
 )
