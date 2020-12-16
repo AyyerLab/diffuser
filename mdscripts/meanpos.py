bf49964a0dcfc399b20efaf93b54c4a723fb1935
Kartik Ayyer <kartik.ayyer@desy.de>
2020-12-16 11:56:17 +0100

    meanpos: Cleaned up

--- mdscripts/meanpos.py
+++ mdscripts/meanpos.py
@@ -1,28 +1,23 @@
-import numpy as np
-import cupy as cp
-import MDAnalysis as md
-import h5py
 import argparse
+import h5py
+import MDAnalysis as md
 
 def main():
-    parser = argparse.ArgumentParser(description = 'Genarating Protein mean position pdb')
-    parser.add_argument('-s', '--tpr', help = 'path to tpr file')
-    parser.add_argument('-f', '--xtc', help = 'path to xtc file')
-    parser.add_argument('-c', '--cov', help = 'path to displacement covariance file')
-    parser.add_argument('-o', '--out_pdb', help = 'path to output pdb file')
-
+    parser = argparse.ArgumentParser(description='Generate Protein mean position pdb')
+    parser.add_argument('-s', '--tpr', help='Path to tpr file')
+    parser.add_argument('-f', '--xtc', help='Path to xtc file')
+    parser.add_argument('-c', '--cov', help='Path to displacement covariance file')
+    parser.add_argument('-o', '--out_pdb', help='Path to output pdb file')
     args = parser.parse_args()
 
-    u = md.Universe(args.tpr, args.xtc) #"data/Lysozyme/lyso_crys.tpr", "data/Lysozyme/lyso295_crys800ns_fit.xtc")
+    univ = md.Universe(args.tpr, args.xtc)
+    atoms = univ.select_atoms("protein")
 
-    p = u.select_atoms("protein")
-    
-    with h5py.File(args.cov, 'r') as f:
-            p_avg = f['mean_pos'][:]
+    with h5py.File(args.cov, 'r') as fptr:
+        p_avg = fptr['mean_pos'][:]
 
-    p.positions = p_avg
-
-    p.write(args.out_pdb)
+    atoms.positions = p_avg
+    atoms.write(args.out_pdb)
 
 if __name__ == '__main__':
     main()
