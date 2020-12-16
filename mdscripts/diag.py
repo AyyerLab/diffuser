20abdd5925f665a830c2276142bf96dcd0cba3db
Kartik Ayyer <kartik.ayyer@desy.de>
2020-12-16 11:56:40 +0100

    diag: Removed CuPy option and satisfied linter

--- mdscripts/diag.py
+++ mdscripts/diag.py
@@ -1,49 +1,37 @@
 import sys
 import argparse
-
 import h5py
-try:
-    import cupy as np
-    CUPY = True
-except ImportError:
-    import numpy as np
-    CUPY = False
+import numpy as np
 
 def get_allcov(cov):
-    allcov = np.zeros((3,cov.shape[1],3,cov.shape[2]))
+    allcov = np.zeros((3, cov.shape[1], 3, cov.shape[2]))
 
-    allcov[0,:,0] = cov[0]
-    allcov[0,:,1] = cov[3].T
-    allcov[0,:,2] = cov[5]
-    allcov[1,:,0] = cov[3]
-    allcov[1,:,1] = cov[1]
-    allcov[1,:,2] = cov[4].T
-    allcov[2,:,0] = cov[5].T
-    allcov[2,:,1] = cov[4]
-    allcov[2,:,2] = cov[2]
+    allcov[0, :, 0] = cov[0]
+    allcov[0, :, 1] = cov[3].T
+    allcov[0, :, 2] = cov[5]
+    allcov[1, :, 0] = cov[3]
+    allcov[1, :, 1] = cov[1]
+    allcov[1, :, 2] = cov[4].T
+    allcov[2, :, 0] = cov[5].T
+    allcov[2, :, 1] = cov[4]
+    allcov[2, :, 2] = cov[2]
 
     return allcov.reshape(cov.shape[1]*3, cov.shape[2]*3)
 
 def main():
-    global CUPY
-    
     parser = argparse.ArgumentParser(description='Diagonizing covariance matrix and saving eigenvectors of atomic displacement')
     parser.add_argument('cov_file', help='Path to covariance file')
     parser.add_argument('-o', '--vecs_fname', help='Path to output vecs file (necessary)')
     parser.add_argument('-n', '--num_vecs', help='Number of eigenvectors to save (default: 100)', type=int, default=100)
-    parser.add_argument('--numpy', help='Force usage of NumPy over CuPy', action='store_true')
     args = parser.parse_args()
 
     if args.vecs_fname is None:
         raise ValueError('Need output filename -o')
-    if args.numpy:
-        import numpy as np
-        CUPY = False
 
     # Loading covariance matrix (6, N, N)
-    with h5py.File(args.cov_file, 'r') as f:
-        allcov = get_allcov(np.array(f['corr'][:]))
-        f0 = np.array(f['f0'][:])
+    with h5py.File(args.cov_file, 'r') as fptr:
+        allcov = get_allcov(np.array(fptr['corr'][:]))
+        f0 = np.array(fptr['f0'][:])
 
     # Diagonalizing (3N, 3N) matrix
     sys.stderr.write('Diagonalizing...')
@@ -64,15 +52,10 @@ def main():
     vec_weights = np.full((args.num_vecs, ), 10.)
 
     # Save to file
-    with h5py.File(args.vecs_fname, 'w') as f:
-        if CUPY:
-            f['vecs']  = vecs_n.get()
-            f['weights'] = vec_weights.get()
-            f['orig_vals'] = vals_n.get()
-        else:
-            f['vecs']  = vecs_n
-            f['weights'] = vec_weights
-            f['orig_vals'] = vals_n
+    with h5py.File(args.vecs_fname, 'w') as fptr:
+        fptr['vecs'] = vecs_n
+        fptr['weights'] = vec_weights
+        fptr['orig_vals'] = vals_n
 
 if __name__ == '__main__':
     main()
