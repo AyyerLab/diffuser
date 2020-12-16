8c533d4fd25f6679592f6b03a2aea56b709e8a35
Kartik Ayyer <kartik.ayyer@desy.de>
2020-12-16 23:12:39 +0100

    __init__: Tracked rename to bgo_optimize

--- diffuser/__init__.py
+++ diffuser/__init__.py
@@ -1,4 +1,4 @@
-from .BGO_optimize import CovarianceOptimizer
+from .bgo_optimize import CovarianceOptimizer
 from .pcdiff import PCDiffuse
 from .rbdiff import RBDiffuse
 from .traj_diff import TrajectoryDiffuse
