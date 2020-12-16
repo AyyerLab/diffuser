70a6e813c594f20f47c391997c4a8f707680c0be
Kartik Ayyer <kartik.ayyer@desy.de>
2020-12-17 00:34:44 +0100

    rbdiff: Cleaned up code

--- diffuser/rbdiff.py
+++ diffuser/rbdiff.py
@@ -1,123 +1,119 @@
 '''Calculate diffuse scattering from rigid body motions of electron density'''
 
 import sys
-import numpy
-try:
-    import cupy as np
-    from cupyx.scipy import ndimage
-    CUPY = True
-    print('Using CuPy')
-    np.disable_experimental_feature_warning = True
-except ImportError:
-    import numpy as np
-    from scipy import ndimage
-    CUPY = False
-    print('Using NumPy/SciPy')
+import numpy as np
+import cupy as cp
+from cupyx.scipy import ndimage
 import mrcfile
 import h5py
 
 class RBDiffuse():
+    '''Calculate diffuse intensities from electron density map and rigid-body parameters'''
     def __init__(self, rot_plane=(1,2)):
         self.size = None
         self.diff_intens = None
         self.rot_plane = rot_plane
+        self.mean_fdens = None
+        self.mean_intens = None
+        self.denominator = None
 
-    def rot_fdens(self, in_fdens, ang):
-        out = np.empty_like(in_fdens)
+    def _rot_fdens(self, in_fdens, ang):
+        out = cp.empty_like(in_fdens)
         ndimage.rotate(in_fdens.real, ang, axes=self.rot_plane, reshape=False, order=0, output=out.real)
         ndimage.rotate(in_fdens.imag, ang, axes=self.rot_plane, reshape=False, order=0, output=out.imag)
         return out
 
-    def trans_fdens(self, in_fdens, vec):
-        return in_fdens * np.exp(-1j * (self.qx*vec[0] + self.qy*vec[1] + self.qz*vec[2]))
+    def _trans_fdens(self, in_fdens, vec):
+        return in_fdens * cp.exp(-1j * (self.qx*vec[0] + self.qy*vec[1] + self.qz*vec[2]))
 
-    def parse(self, fname, reset=True, **kwargs):
+    def parse(self, fname):
+        '''Parse electron density from file'''
         with mrcfile.open(fname, 'r') as f:
-            self.dens = np.array(f.data)
+            self.dens = cp.array(f.data)
             self.cella = f.header.cella['x']
-        self.fdens = np.fft.fftshift(np.fft.fftn(np.fft.ifftshift(self.dens)))
-        if reset:
-            self.initialize(**kwargs)
+        self.fdens = cp.fft.fftshift(cp.fft.fftn(cp.fft.ifftshift(self.dens)))
 
-    def initialize(self, translate=True):
-        self.mean_fdens = np.zeros_like(self.fdens)
-        self.mean_intens = np.zeros_like(self.fdens, dtype='f4')
+    def init_diffcalc(self, translate=True):
+        '''Initialize accumulation of 1st and 2nd moments for diffuse intensity calculation'''
+        self.mean_fdens = cp.zeros_like(self.fdens)
+        self.mean_intens = cp.zeros_like(self.fdens, dtype='f4')
         self.denominator = 0.
 
         if translate and self.size != self.fdens.shape[0]:
             self.size = self.fdens.shape[0]
             cen = self.size // 2
-            self.qx, self.qy, self.qz = np.indices(self.fdens.shape, dtype='f4')
-            self.qx = (self.qx - cen) / self.size * 2. * np.pi
-            self.qy = (self.qy - cen) / self.size * 2. * np.pi
-            self.qz = (self.qz - cen) / self.size * 2. * np.pi
+            self.qx, self.qy, self.qz = cp.indices(self.fdens.shape, dtype='f4')
+            self.qx = (self.qx - cen) / self.size * 2. * cp.pi
+            self.qy = (self.qy - cen) / self.size * 2. * cp.pi
+            self.qz = (self.qz - cen) / self.size * 2. * cp.pi
 
-    def run_mc(self, num_steps, sigma_deg, cov_vox, prefix=''):
-        shifts = np.array(numpy.random.multivariate_normal(numpy.zeros(3), cov_vox, size=num_steps))
-        angles = np.random.randn(num_steps) * sigma_deg
-        weights = np.ones(shifts.shape[0])
+    def run_mc(self, num_steps, sigma_deg, cov_vox, prefix='', reset=False):
+        '''Run Monte Carlo sampling of rigid body motions to get diffuse intensity'''
+        shifts = cp.array(np.random.multivariate_normal(np.zeros(3), cov_vox, size=num_steps))
+        angles = cp.random.randn(num_steps) * sigma_deg
+        if reset:
+            self.init_diffcalc(translate=True)
 
         for i in range(num_steps):
             if shifts[i].max() != 0.:
-                modfdens = self.trans_fdens(self.fdens, tuple(shifts[i]))
+                modfdens = self._trans_fdens(self.fdens, tuple(shifts[i]))
             else:
                 modfdens = self.fdens
 
             if angles[i] != 0.:
-                modfdens = self.rot_fdens(modfdens, angles[i])
+                modfdens = self._rot_fdens(modfdens, angles[i])
 
             self.mean_fdens += modfdens
-            self.mean_intens += np.abs(modfdens)**2
+            self.mean_intens += cp.abs(modfdens)**2
             self.denominator += 1
 
             sys.stderr.write('\r%s%d/%d      ' % (prefix, i+1, num_steps))
-        #sys.stderr.write('\n')
+        sys.stderr.write('\n')
 
-    def rotate_weighted(self, num_steps, sigma_deg):
-        angles = np.linspace(-3*sigma_deg, 3*sigma_deg, num_steps)
-        weights = np.exp(-angles**2/2./sigma_deg**2)
+    def rotate_weighted(self, num_steps, sigma_deg, reset=True):
+        '''Get diffuse intensities by rotating object about single axis'''
+        angles = cp.linspace(-3*sigma_deg, 3*sigma_deg, num_steps)
+        weights = cp.exp(-angles**2/2./sigma_deg**2)
+        if reset:
+            self.init_diffcalc(translate=False)
+
         for i in range(num_steps):
-            rotfdens = self.rot_fdens(self.fdens, angles[i])
+            rotfdens = self._rot_fdens(self.fdens, angles[i])
             self.mean_fdens += rotfdens * weights[i]
-            self.mean_intens += np.abs(rotfdens)**2 * weights[i]
+            self.mean_intens += cp.abs(rotfdens)**2 * weights[i]
             self.denominator += weights[i]
             sys.stderr.write('\r%d/%d: %+.3f deg (%.3e)   ' % (i+1, num_steps, angles[i], weights[i]))
         sys.stderr.write('\n')
 
     def aggregate(self):
+        '''Finish accumulation and calculate diffuse intensities'''
+        if self.denominator is None or self.denominator == 0.:
+            raise ValueError('Run a few steps before aggregating intensities')
         self.mean_fdens /= self.denominator
         self.mean_intens /= self.denominator
 
-        self.diff_intens = self.mean_intens - np.abs(self.mean_fdens)**2
-        # If using GPU, move output back to host
-        if CUPY:
-            self.diff_intens = self.diff_intens.get()
+        self.diff_intens = self.mean_intens - cp.abs(self.mean_fdens)**2
+        self.diff_intens = self.diff_intens.get()
 
     def save(self, out_fname):
+        '''Save to given HDF5 file'''
         if self.diff_intens is None:
             self.aggregate()
-
-
-        # Save to file
-        with h5py.File(out_fname, 'w') as f:
-            f.create_dataset('diff_intens', data=self.diff_intens.astype('f4'))
-
-#        with mrcfile.new(out_fname, overwrite=True) as f:
-#            f.set_data(self.diff_intens.astype('f4'))
-#            for key in f.header.cella.dtype.names:
-#                f.header.cella[key] = self.cella
+        with h5py.File(out_fname, 'w') as fptr:
+            fptr['diff_intens'] = self.diff_intens.astype('f4')
 
 def main():
+    '''Run with test data (example code)'''
     fname = 'data/2w5j_cutout_rot_remap.ccp4'
     out_fname = 'data/2w5j_diffcalc.ccp4'
     sigma_deg = 7
     sigma_vox = 0.8
     rot_plane = (1, 2) # Rotate about x-axis
     num_steps = 21
-    cov_vox = numpy.identity(3)*sigma_vox**2
+    cov_vox = np.identity(3)*sigma_vox**2
 
     # Instantiate class
-    rbd = RBDiffuse()
+    rbd = RBDiffuse(rot_plane=rot_plane)
     # Parse electron density
     rbd.parse(fname)
     # Run MC calculation with translation and rotation
