deb6b88d81061e8c7b27b5e0e92b050391baaeed
Kartik Ayyer <kartik.ayyer@desy.de>
2020-12-16 23:47:14 +0100

    bgo_optimize: Cleaned up and satisfied linter

--- diffuser/bgo_optimize.py
+++ diffuser/bgo_optimize.py
@@ -1,27 +1,26 @@
-import sys
 import configparser
 import numpy as np
 import cupy as cp
-from cupyx.scipy import ndimage as cndimage
 import h5py
 import skopt
-from skopt import forest_minimize
-from skopt import callbacks
 from skopt.callbacks import CheckpointSaver
-import pcdiff
+
+from diffuser import PCDiffuse
 
 class CovarianceOptimizer():
+    '''Class to run Bayesian optimization to get best covariance parameters
+    matching the calculated and target diffuse scattering
+    '''
     def __init__(self, config_file):
         conf = configparser.ConfigParser()
         conf.read(config_file)
-        with h5py.File(conf.get('optimizer', 'itarget_fname'), 'r') as f:
-            #self.Itarget = cp.array(f['diff_intens'][:])
-            self.Itarget = f['diff_intens'][:]
+        with h5py.File(conf.get('optimizer', 'itarget_fname'), 'r') as fptr:
+            self.i_target = fptr['diff_intens'][:]
 
         # Setup PCDiff...
         self.num_steps = conf.getint('optimizer', 'num_steps')
         self.num_vecs = conf.getint('optimizer', 'num_vecs')
-        self.pcd = pcdiff.PCDiffuse(config_file)
+        self.pcd = PCDiffuse(config_file)
         self.pcd.num_steps = self.num_steps
         self.pcd.num_vecs = self.num_vecs
         self.pcd.cov_weights = cp.identity(self.num_vecs)
@@ -46,45 +45,46 @@ class CovarianceOptimizer():
         if self.point_group not in ['1', '222']:
             raise ValueError('%s point group not implemented' % self.point_group)
         self.dims = []
-        self.get_dims()
+        self._get_dims()
 
         if intern_fname is not None:
-            with h5py.File(intern_fname, 'r') as f:
-                self.Iintern = cp.array(f['diff_intens'][:])
+            with h5py.File(intern_fname, 'r') as fptr:
+                self.i_intern = cp.array(fptr['diff_intens'][:])
         else:
-            self.Iintern = None
+            self.i_intern = None
 
-        self.intrad, self.voxmask = self.get_qsel(*qrange)
-        self.voxmask &= ~np.isnan(self.Itarget)
+        self.intrad, self.voxmask = self._get_qsel(*qrange)
+        self.voxmask &= ~np.isnan(self.i_target)
         self.radcount = None
         if self.do_aniso:
-            radavg = self.get_radavg(self.Itarget)
-            self.Itarget -= radavg[self.intrad]
+            radavg = self._get_radavg(self.i_target)
+            self.i_target -= radavg[self.intrad]
 
     def optimize(self, num_iter, resume=False, n_initial_points=10, **kwargs):
+        '''Run BGO optimizer to find optimal parameters to fit agains target diffuse'''
         if resume:
             cres = skopt.load(self.output_fname)
-            x0 = cres.x_iters
-            y0 = cres.func_vals
+            x_init = cres.x_iters
+            y_init = cres.func_vals
         else:
-            x0 = None
-            y0 = None
+            x_init = None
+            y_init = None
 
         checkpoint_saver = CheckpointSaver(self.output_fname, store_objective=False)
-        res = skopt.gp_minimize(self.obj_fun,
+        skopt.gp_minimize(self.obj_fun,
             self.dims,
             n_calls = num_iter,
             n_random_starts = n_initial_points,
             callback = [checkpoint_saver],
             noise = 1e-7,
             verbose = True,
-            x0 = x0,
-            y0 = y0,
+            x0 = x_init,
+            y0 = y_init,
             **kwargs
             )
 
-    def get_dims(self):
-        db = self.diag_bounds
+    def _get_dims(self):
+        ddb = self.diag_bounds
         odb = self.offdiag_bounds
         llmsb = self.llm_sigma_bounds
         llmgb = self.llm_gamma_bounds
@@ -93,7 +93,7 @@ class CovarianceOptimizer():
         rbr = self.rbr_bounds
 
         self.dims_code = 0 # No optimization
-        if  db[1] - db[0] != 0:
+        if ddb[1] - ddb[0] != 0:
             self.dims_code += 1
             print('Optimizing diagonal components')
         if odb[1] - odb[0] != 0:
@@ -115,7 +115,7 @@ class CovarianceOptimizer():
         for i in range(self.num_vecs):
             for j in range(i+1):
                 if i == j and self.dims_code & 1 != 0:
-                    self.dims += [skopt.space.Real(*db, name='X_%d_%d'%(i,j))]
+                    self.dims += [skopt.space.Real(*ddb, name='X_%d_%d'%(i,j))]
                 elif self.dims_code & 2 != 0:
                     self.dims += [skopt.space.Real(*odb, name='X_%d_%d'%(i,j))]
 
@@ -134,83 +134,77 @@ class CovarianceOptimizer():
 
         print(len(self.dims), 'dimensional optimization')
 
-    def get_mc_intens(self, s):
-        '''Get MC diffuse intensities for given 's' parameter'''
-        #get s values from 5vecs_diag_optimization
-        ''' res_5vecs_diag = skopt.load('../CypA/xtc/md295_forest_md_5vecs_diag.pkl')
-        sigmas_diag = cp.array(res_5vecs_diag.x)
-        self.pcd.cov_weights = self.pcd.cov_weights * sigmas_diag**2
-        '''
-
+    def get_mc_intens(self, svec):
+        '''Get MC diffuse intensities for given s-vector'''
         # Set up MC parameters
         self.pcd.cov_weights[:] = 0.
         n = 0
         for i in range(self.num_vecs):
             for j in range(i+1):
                 if i == j and self.dims_code & 1 != 0:
-                    self.pcd.cov_weights[i, j] = s[n]**2
+                    self.pcd.cov_weights[i, j] = svec[n]**2
                     n += 1
                 elif self.dims_code & 2 != 0:
-                    self.pcd.cov_weights[i, j] = s[n]
-                    self.pcd.cov_weights[j, i] = s[n]
+                    self.pcd.cov_weights[i, j] = svec[n]
+                    self.pcd.cov_weights[j, i] = svec[n]
                     n += 1
 
         if self.dims_code & 4 != 0:
-            self.pcd.sigma_uncorr_A = s[n]
+            self.pcd.sigma_uncorr = svec[n]
             n += 1
         if self.dims_code & 8 != 0:
-            self.pcd.cov_vox = s[n]**2 * np.identity(3)
+            self.pcd.cov_vox = svec[n]**2 * np.identity(3)
             n += 1
         if self.dims_code & 16 != 0:
-            self.pcd.sigma_deg = s[n]
+            self.pcd.sigma_deg = svec[n]
             n += 1
 
         # Calculate MC intensity if needed
         if self.dims_code % 32 != 0:
             self.pcd.run_mc()
-            Imc = self.pcd.diff_intens
-        elif self.Iintern is not None:
-            Imc = self.Iintern
+            i_mc = self.pcd.diff_intens
+        elif self.i_intern is not None:
+            i_mc = self.i_intern
         else:
-            Imc = self.pcd.get_intens()
+            i_mc = self.pcd.get_intens()
 
         # Apply LLM transforms
         if self.dims_code & 32 != 0:
             if self.lattice_llm:
-                Icalc = self.pcd.liqlatt(s[-2], s[1]) * Imc
+                i_calc = self.pcd.liqlatt(svec[-2], svec[1]) * i_mc
             else:
-                Icalc = self.pcd.liquidize(Imc, s[-2], s[-1])
+                i_calc = self.pcd.liquidize(i_mc, svec[-2], svec[-1])
         else:
-            Icalc = Imc
+            i_calc = i_mc
 
         if self.point_group == '222':
-            Icalc = 0.25 * (Icalc + Icalc[::-1] + Icalc[:,::-1] + Icalc[:,:,::-1])
+            i_calc = 0.25 * (i_calc + i_calc[::-1] + i_calc[:,::-1] + i_calc[:,:,::-1])
 
-        return Icalc.get()
+        return i_calc.get()
 
-    def obj_fun (self, s):
-        '''Calcuates L2-norm between MC diffuse with given 's' and target diffuse'''
-        Icalc = self.get_mc_intens(s)
+    def obj_fun (self, svec):
+        '''Calcuates (1 - CC) between calculated diffuse with given s-vector and target diffuse'''
+        i_calc = self.get_mc_intens(svec)
 
         if self.do_aniso:
-            radavg = self.get_radavg(Icalc)
-            Icalc -= radavg[self.intrad]
+            radavg = self._get_radavg(i_calc)
+            i_calc -= radavg[self.intrad]
 
         if self.do_weighting:
-            cov = np.cov(Icalc[self.voxmask], self.Itarget[self.voxmask], aweights=1./self.intrad[self.voxmask]**2)
+            cov = np.cov(i_calc[self.voxmask], self.i_target[self.voxmask], aweights=1./self.intrad[self.voxmask]**2)
             retval = 1. - cov[0, 1] / np.sqrt(cov[0,0] * cov[1,1])
         else:
-            retval = 1. - np.corrcoef(Icalc[self.voxmask], self.Itarget[self.voxmask])[0,1]
+            retval = 1. - np.corrcoef(i_calc[self.voxmask], self.i_target[self.voxmask])[0,1]
 
         return float(retval)
 
-    def get_qsel(self, qmin, qmax, binsize=None):
-        cen = self.size // 2
-        binsize = self.pcd.qrad[0,0,0] - self.pcd.qrad[0,0,1]
+    def _get_qsel(self, qmin, qmax, binsize=None):
+        if binsize is None:
+            binsize = self.pcd.qrad[0,0,0] - self.pcd.qrad[0,0,1]
         binrad = (self.pcd.qrad / binsize).get().astype('i4')
         return binrad, ((self.pcd.qrad >= qmin) & (self.pcd.qrad <= qmax)).get()
 
-    def get_radavg(self, intens):
+    def _get_radavg(self, intens):
         if self.radcount is None:
             self.radcount = np.zeros(self.intrad.max() + 1)
             np.add.at(self.radcount, self.intrad[self.voxmask], 1)
@@ -234,4 +228,3 @@ if __name__ == '__main__':
 
     opt = CovarianceOptimizer(args.config_file)
     opt.optimize(args.num_iter, resume=args.resume, n_initial_points=args.initial_points)
-
