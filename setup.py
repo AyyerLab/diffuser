ae63ba2c7d200c875cc2efdca27f01f70dfff42b
Kartik Ayyer <kartik.ayyer@desy.de>
2021-02-04 13:12:23 +0100

    setup: Added dependencies except CuPy which should be installed separately based on CUDA version

--- setup.py
+++ setup.py
@@ -3,6 +3,16 @@ from setuptools import setup
 setup(name = 'diffuser',
     version = '0.1',
     packages = ['diffuser'],
+    install_requires = [
+        'numpy',
+        'h5py',
+        'scipy',
+        'scikit-optimize',
+        'mrcfile',
+        'mock', # Till MDAnalysis issue is fixed for python 3
+        'MDAnalysis',
+        'matplotlib',
+    ],
     entry_points = {'console_scripts': [
         'diffuser.bgo_optimize = diffuser.bgo_optimize:main',
         'diffuser.pcdiff = diffuser.pcdiff:main',
