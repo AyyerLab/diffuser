772f3761936f3813686d751d3cacc83b5d93e9ec
Kartik Ayyer <kartik.ayyer@desy.de>
2021-02-05 09:41:01 +0100

    calc_cc: Moved to package directory and satisfied linter

--- diffuser/calc_cc.py
+++ diffuser/calc_cc.py
@@ -1,33 +1,36 @@
+import argparse
 import csv
-import mrcfile
-import numpy
+
 import h5py
 try:
     import cupy as np
-    from cupyx.scipy import ndimage
     import cupyx
     CUPY = True
     np.disable_experimental_feature_warning = True
     scatter_add = cupyx.scatter_add
 except ImportError:
     import numpy as np
-    from scipy import ndimage
     CUPY = False
     scatter_add = np.add.at
 
-def calc_rad(size, binning):
+def calc_rad(size, binning, return_float=False):
+    '''Calculate radius and binned radius of 3D grid of voxels with given size'''
     cen = size // 2
     ind = np.arange(size, dtype='f4') - cen
     x, y, z = np.meshgrid(ind, ind, ind, indexing='ij')
     rad = np.sqrt(x*x + y*y + z*z)
-    return rad, np.rint(rad / binning).astype('i4')
+
+    if return_float:
+        return rad
+    return np.rint(rad / binning).astype('i4')
 
 def subtract_radavg(vol, intrad, num_bins, mask=None):
+    '''Subtract radial average of signal from binned radii'''
     radavg = np.zeros(num_bins)
     radcount = np.zeros(num_bins)
     if mask is None:
         mask = ~np.isnan(vol)
-    
+
     scatter_add(radcount, intrad[mask], 1)
     scatter_add(radavg, intrad[mask], vol[mask])
     sel = (radcount > 0)
@@ -37,6 +40,7 @@ def subtract_radavg(vol, intrad, num_bins, mask=None):
     return radavg
 
 def calc_cc(vol1, vol2, intrad, num_bins, mask=None):
+    '''Calculate CC as a function of binned radius'''
     v1v2 = np.zeros(num_bins)
     v1v1 = np.zeros(num_bins)
     v2v2 = np.zeros(num_bins)
@@ -53,19 +57,20 @@ def calc_cc(vol1, vol2, intrad, num_bins, mask=None):
     return v1v2
 
 def save_to_file(fname, cc, q):
+    '''Save CC vs q data to text file'''
     print('Writing output to', fname)
-    with open(fname, 'w') as f:
-        w = csv.writer(f, delimiter='\t')
+    with open(fname, 'w') as fptr:
+        writer = csv.writer(fptr, delimiter='\t')
         if q is not None:
-            w.writerow(['Radius', 'q (1/A)', 'CC'])
-            w.writerows(zip(np.arange(len(cc)), q, cc))
+            writer.writerow(['Radius', 'q (1/A)', 'CC'])
+            writer.writerows(zip(np.arange(len(cc)), q, cc))
         else:
             print('Resolution at edge not provided so CC only given vs radius')
-            w.writerow(['Radius', 'CC'])
-            w.writerows(zip(np.arange(len(cc)), cc))
+            writer.writerow(['Radius', 'CC'])
+            writer.writerows(zip(np.arange(len(cc)), cc))
 
 def main():
-    import argparse
+    '''CLI entry point. Also shows usage pattern'''
     parser = argparse.ArgumentParser(description='CC vs radius/q calculator')
     parser.add_argument('volume1', help='Path to first volume ccp4 map')
     parser.add_argument('volume2', help='Path to second volume ccp4 map')
@@ -73,21 +78,17 @@ def main():
     parser.add_argument('-b', '--bin_size', help='Radial bin size in voxels. Default: 1', type=int, default=1)
     parser.add_argument('-o', '--out_fname', help='Path to output file. Default: cc.dat', default='cc.dat')
     args = parser.parse_args()
-    
-    #with mrcfile.open(args.volume1, 'r') as f:
-    #    vol1 = np.array(np.copy(f.data))
-    with h5py.File(args.volume1, 'r') as f:
-        vol1 = np.array(f['diff_intens'][:])
-    #with mrcfile.open(args.volume2, 'r') as f:
-    #    vol2 = np.array(np.copy(f.data))
-    with h5py.File(args.volume2, 'r') as f:
-        vol2 = np.array(f['diff_intens'][:])
+
+    with h5py.File(args.volume1, 'r') as fptr:
+        vol1 = np.array(fptr['diff_intens'][:])
+    with h5py.File(args.volume2, 'r') as fptr:
+        vol2 = np.array(fptr['diff_intens'][:])
     mask = ~(np.isnan(vol1) | np.isnan(vol2))
-    
+
     assert vol1.shape == vol2.shape
     size = vol1.shape[-1]
 
-    rad, rbin = calc_rad(size, args.bin_size)
+    rbin = calc_rad(size, args.bin_size)
     num_bins = int(rbin.max() + 1)
 
     subtract_radavg(vol1, rbin, num_bins, mask)
